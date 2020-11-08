16b85078017a5a45631835e434df6e05cea1ea6a
Jiří Dokoupil <33521168+jiridoko@users.noreply.github.com>
2020-11-08 10:27:10 +0000

    latest nozzle offset and extruder updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0, 80.0, 397.29, 473.09 } //417.39
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0, 80.0, 397.29, 400.00 } //417.39
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -995,7 +995,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -3, 16, -2.03 }
+#define NOZZLE_TO_PROBE_OFFSET { -3, 16, -2.151 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
